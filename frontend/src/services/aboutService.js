0687b814426bc02ee5fad824882b2e61710a1f8a
Bevherly Juvhel ELONDA <bejuvhelonda@gmail.com>
2026-08-15 11:52:51 +0100

    Update aboutService.js and add the real const API_URL

--- frontend/src/services/aboutService.js
+++ frontend/src/services/aboutService.js
@@ -1,5 +1,5 @@
-//const API_URL = `${import.meta.env.VITE_API_URL}/api/about/`;
-const API_URL = "http://127.0.0.1:8000/api/about/";
+const API_URL = `${import.meta.env.VITE_API_URL}/api/about/`;
+//const API_URL = "http://127.0.0.1:8000/api/about/";
 
 export async function getAbout() {
   const response = await fetch(API_URL);
